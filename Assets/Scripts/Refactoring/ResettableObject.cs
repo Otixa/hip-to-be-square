4c7433b8e09285640ad86bd49daef3697f13a81e
cymrodan <cymrodan@gmail.com>
2017-08-10 22:44:05 +0100

    Created ResettableObject class

---
+++ Assets/Scripts/Refactoring/ResettableObject.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ResettableObject : MonoBehaviour {
+    /// <summary>
+    /// The original start position of the Object when firs
+    /// </summary>
+    public Vector3 resetPosition;                   //as will need to access this from object pooler when positions are changing as objects are spawned and destroyed
+    public bool shouldReset = true;                 //for special situation where we want a resettableObject to behave differently 
+
+    //this will be set 
+    protected virtual void OnReset() { }
+
+    // Use this for initialization
+    private void Start () {         //First enabled                             (//Awake when initialised, regardless of being enabled or not)
+        resetPosition = transform.position;
+	}
+
+    private void OnEnable()
+    {
+        //Set current position to be the stored startingPosition (which is when the object is instantiated
+        if (shouldReset && resetPosition != transform.position)
+        {
+            transform.position = resetPosition;
+            OnReset();
+        }
+        resetPosition = transform.position;
+    }
+
+    
+
+}
